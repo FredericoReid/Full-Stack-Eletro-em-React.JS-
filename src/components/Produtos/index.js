8e449320a172945e02e444a8638d6867fd656e19
FredericoReid <fredericoreid@hotmail.com>
2020-12-12 21:45:49 -0300

    Reparado link da api de produtos na página principal

--- src/components/Produtos/index.js
+++ src/components/Produtos/index.js
@@ -5,9 +5,29 @@ const Produtos = () => {
     const [produtos, setProdutos] = useState([]);
 
     useEffect(async() => {
-        const result = await fetch("http://localhost:8080/React6/fse/src/php/produtosapi.php");
+        const result = await fetch("http://localhost:8080/Full Stack Eletro React.JS - Ultima Versão/src/php/produtosapi.php");
         setProdutos(await result.json())
     }, [])
+
+let destaque = (event) => {
+    if (event.target.style.width === "260px") {
+        event.target.style.width = "120px";
+    }
+    else {
+        event.target.style.width = "260px";
+        event.target.style.height = "260px"
+    }
+}
+
+let redimensiona = (event) => {
+    if (event.target.style.width === "120px") {
+        event.target.style.width = "260px";
+    }
+    else {
+        event.target.style.width = "120px";
+        event.target.style.height = "120px"
+    }
+}
 
 
     return (
@@ -20,7 +40,8 @@ const Produtos = () => {
                                 
                                 <div className="col-sm-3">
                                     <div key={value.id}>
-                                        <img src= {value.imagem} alt="a" width="120" height="120" /> 
+                                        <img src= {value.imagem} alt="a" width="120" height="120" onMouseOver={destaque}
+                                            onMouseOut={redimensiona}/> 
                                         <h5>{value.descricao}</h5>
                                         <h5>R$:{value.preco}</h5>
                                         <br /> 
