5fffecef98ade608e659214cb3500dc62fcb3975
FredericoReid <fredericoreid@hotmail.com>
2020-12-12 21:46:14 -0300

    Criado o componente Rodapé

---
+++ src/components/Rodape/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Rodape(){
+    return (
+        <>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-12">
+                        <div className="text-center"><h3> Formas de Pagamento: </h3></div>
+                        <div className="text-center"><img src="./imagens/form pagamento.jpeg" alt="forma de pagamento" width="800" height="100" /></div>
+                    </div>
+                </div>
+            </div>
+            
+        </>
+    )
+}
+
+export default Rodape;
