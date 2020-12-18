a38eb0a1bcfa0d3ba65f7359385934603132323f
FredericoReid <fredericoreid@hotmail.com>
2020-12-17 23:06:22 -0300

    Reestruturação das formas de pagamento

--- src/components/Rodape/index.js
+++ src/components/Rodape/index.js
@@ -1,16 +1,29 @@
 import React from 'react';
+import './styles.css';
 
 function Rodape(){
     return (
         <>
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="col-12">
-                        <div className="text-center"><h5> Formas de Pagamento: </h5></div>
-                        <div className="text-center"><img src="./imagens/form pagamento.jpeg" alt="forma de pagamento" width="800" height="80" /></div>
+            <div className="rodape">
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-sm-12 text-center" id="formas-pagamento"><h5> Formas de Pagamento: </h5></div>
+                        <div className="col-sm-12 text-center">
+                            <div className="imagens">
+                                <img src="./imagens/mastercard.jpeg" id="imagem-cartao" alt="forma de pagamento" width="50" height="50" />
+                                <img src="./imagens/visa.jpeg" id="imagem-cartao" alt="forma de pagamento" width="50" height="50" />
+                                <img src="./imagens/maestro.jpeg" id="imagem-cartao" alt="forma de pagamento" width="50" height="50" />
+                                <img src="./imagens/bitcoin.jpeg" id="imagem-cartao" alt="forma de pagamento" width="50" height="50" />
+                                <img src="./imagens/paypal.jpeg" id="imagem-cartao" alt="forma de pagamento" width="50" height="50" />
+                                <img src="./imagens/discover.jpeg" id="imagem-cartao" alt="forma de pagamento" width="50" height="50" />
+                                <img src="./imagens/amazon.jpeg" id="imagem-cartao" alt="forma de pagamento" width="50" height="50" />
+                                <img src="./imagens/apple.jpeg" id="imagem-cartao" alt="forma de pagamento" width="50" height="50" />
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
+            <br />
             
         </>
     )
